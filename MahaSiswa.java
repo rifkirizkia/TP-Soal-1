90a79daa159cec2a879653aafa0ce376dcd97df1
Rifki Rizkia <rifkirizkia91@gmail.com>
2020-09-19 13:30:33 +0700

    MahaSiswa.java

---
+++ MahaSiswa.java
@@ -0,0 +1,15 @@
+public class MahaSiswa {
+	public static void main (String[] args) {
+		//uang Dira yang akan ditabung sebesar 13500 dan akan di tabung selama 40 hari
+		int UANG_DIRA = 13500;
+		int LAMA_MENABUNG = 40;
+		
+		//total dari semua tabungan Dira
+		int TOTAL_SEMUA = UANG_DIRA * LAMA_MENABUNG;
+		
+		System.out.println("Uang tabungan Dira selama 40 hari");
+		System.out.println("Uang Dira = Rp.13500");
+		System.out.println("Lama menabung = 40 hari");
+		System.out.println("Total uang Dira setelah menabung = Rp." + TOTAL_SEMUA);
+	}
+}
